5f12b052b94bdaf03bf384b3fb9c7c7504367ac8
estanislaodiego <ediego@mail.austral.edu.ar>
2026-03-17 10:32:21 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,3 +3,11 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+    nombre = input("ingresar nombre")
+    apellido = input("ingresar apellido")
+    nombre_y_apellido = (nombre + " " + apellido)
+
+    print (nombre_y_apellido. lower())
+    print (nombre_y_apellido. title())
+    print (nombre_y_apellido. upper())
+    print ("\t" + nombre_y_apellido. lower())
