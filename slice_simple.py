5cd6b89ee9d1c257ab57bb96b092f0b1a8d48871
estanislaodiego <ediego@mail.austral.edu.ar>
2026-03-17 17:32:46 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,4 +2,7 @@ def slice_simple():
     """Dado el texto 'Awesome', imprime distintos substrings
     usando slicing y lower().
     """
-    texto = "Awesome"
+    texto = "Awesome".lower()
+    print(texto[:3])
+    print(texto[2:5])
+    print(texto[:7])
